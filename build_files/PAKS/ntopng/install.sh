1dd2e9e2b12643ff290605daa197ff4a6f088879
ummeegge <ummeegge@ipfire.org>
2017-10-02 10:40:21 +0200

    Initial push for ntopng integration into IPFire.

---
+++ build_files/PAKS/ntopng/install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+############################################################################
+#                                                                          #
+# This file is part of the IPFire Firewall.                                #
+#                                                                          #
+# IPFire is free software; you can redistribute it and/or modify           #
+# it under the terms of the GNU General Public License as published by     #
+# the Free Software Foundation; either version 2 of the License, or        #
+# (at your option) any later version.                                      #
+#                                                                          #
+# IPFire is distributed in the hope that it will be useful,                #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+# GNU General Public License for more details.                             #
+#                                                                          #
+# You should have received a copy of the GNU General Public License        #
+# along with IPFire; if not, write to the Free Software                    #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA #
+#                                                                          #
+# Copyright (C) 2007 IPFire-Team <info@ipfire.org>.                        #
+#                                                                          #
+############################################################################
+#
+. /opt/pakfire/lib/functions.sh
+NAME="ntopng";
+extract_files
+restore_backup ${NAME}
+
+# Delete old symlink if presant
+rm -rfv /etc/rc.d/rc?.d/???${NAME};
+
+## Add symlinks
+# Possible runlevel ranges
+SO="[2-5][0-9]";
+SA="[4-7][0-9]";
+RE="[2-5][0-9]";
+# Search free runlevel
+STOP=$(ls /etc/rc.d/rc0.d/ | sed -e 's/[^0-9]//g' | awk '$1!=p+1{print p+1" "$1-1}{p=$1}' | sed -e '1d' | tr ' ' '\n' | grep -E "${SO}" | head -1);
+START=$(ls /etc/rc.d/rc3.d/ | sed -e 's/[^0-9]//g' | awk '$1!=p+1{print p+1" "$1-1}{p=$1}' | sed -e '1d' | tr ' ' '\n' | grep -E "${SA}" | head -1);
+REBOOT=$(ls /etc/rc.d/rc6.d/ | sed -e 's/[^0-9]//g' | awk '$1!=p+1{print p+1" "$1-1}{p=$1}' | sed -e '1d' | tr ' ' '\n' | grep -E "${RE}" | head -1);
+## Add symlinks 
+ln -s ../init.d/${NAME} /etc/rc.d/rc0.d/K${STOP}${NAME};
+ln -s ../init.d/${NAME} /etc/rc.d/rc3.d/S${START}${NAME};
+ln -s ../init.d/${NAME} /etc/rc.d/rc6.d/K${REBOOT}${NAME};
+
+# Disable mprotect for ntopng otherwise it won´t start
+paxctl -c /usr/bin/ntopng
+paxctl -pemrxs /usr/bin/ntopng
+
+# EOF
