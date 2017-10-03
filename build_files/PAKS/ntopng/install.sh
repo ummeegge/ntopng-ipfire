1d45643add7c4650f23fa7669181d9cb448c16f3
ummeegge <ummeegge@ipfire.org>
2017-10-03 11:36:10 +0200

    Added automatic configuration to start ntopng after install
    - Added automatic setup of certificate for HTTPS access and entry for configuration file.
    - Added automatic interface detection and entry for configuration file.
    - Added automatic subnet detection and entry for configuration file.
    - Disabled mprotect for ntopng binary via installation.
    - Start ntopng after installation automatically.
    - Deleted make install for nDPI.
    - Adapted ROOTFILE.

--- build_files/PAKS/ntopng/install.sh
+++ build_files/PAKS/ntopng/install.sh
@@ -23,14 +23,31 @@
 #
 . /opt/pakfire/lib/functions.sh
 NAME="ntopng";
+CONF="/etc/ntopng/ntopng.conf";
+SSL="/usr/share/ntopng/httpdocs/ssl";
+SETTINGS="/var/ipfire/ethernet/settings";
+
 extract_files
 restore_backup ${NAME}
+
+# Formatting and Colors
+COLUMNS="$(tput cols)";
+R=$(tput setaf 1);
+Y=$(tput setaf 3);
+B=$(tput setaf 6);
+N=$(tput sgr0);
+seperator(){ printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -; };
+
+clear;
 
 # Delete old symlink if presant
 rm -rfv /etc/rc.d/rc?.d/???${NAME};
 
 ## Add symlinks
 # Possible runlevel ranges
+echo;
+echo "${Y}Will set symlinks to activate the initscript for start|stop|reboot${N}";
+seperator;
 SO="[2-5][0-9]";
 SA="[4-7][0-9]";
 RE="[2-5][0-9]";
@@ -44,7 +61,86 @@ ln -s ../init.d/${NAME} /etc/rc.d/rc3.d/S${START}${NAME};
 ln -s ../init.d/${NAME} /etc/rc.d/rc6.d/K${REBOOT}${NAME};
 
 # Disable mprotect for ntopng otherwise it won´t start
-paxctl -c /usr/bin/ntopng
-paxctl -pemrxs /usr/bin/ntopng
+echo;
+echo "${Y}Disable mprotect for ntopng${N}";
+seperator;
+paxctl -c /usr/bin/ntopng 2>/dev/null;
+paxctl -pemrxs /usr/bin/ntopng 2>/dev/null;
+
+# Investigate existing subnets
+echo;
+echo "${Y}Add subnets to ntopng configuration file${N}";
+seperator;
+ADDRESSES=$(netstat -r | awk '/red/||/green/||/blue/||/orange/ { if ($1 ~ /^[0-9]/) print $1"/"$3}' | tr '\n' ',' | head -c-1);
+# Investigate existing interfaces
+INTERFACE=$(for i in $(ifconfig | awk '/green/ || /blue/ || /orange/ || /ppp/ || /red/ || /tun/ { print $1 }'); do
+	echo "--interface ${i}";
+done)
+# Paste investigated networks into ntopng.conf if no entries exist
+if ! grep -q "# Local Networks:" ${CONF}; then
+	cat >> ${CONF} <<EOF
+
+#
+# Local Networks:
+# ===== =========
+# ***Note: To add more than one local network comma separate:
+# --local-networks 172.16.1.0/24,172.31.1.0/24
+$(echo "--local-networks ${ADDRESSES}")
+EOF
+
+else
+	echo "There are already entries for your '--local-networks'. Please check it by your own under ${CONF}... ";
+fi
+# Paste investigated subnets into ntopng.conf if no entries exist
+echo
+echo "${Y}Add interfaces to ntopng configuration file${N}";
+seperator;
+if ! grep -q "# Network Interface(s):" ${CONF}; then
+	cat >> ${CONF} <<EOF
+
+#
+# Network Interface(s):
+# ======= =============
+# ***Note: To add more than one interface use multiple entries:
+#--interface p3p1
+#--interface p3p2
+${INTERFACE}
+EOF
+
+else
+	echo "There are already entries for your '--interface'. Please check it by your own under ${CONF}... ";
+fi
+
+## Thanks gocart ;-)
+# create https cert
+echo;
+echo "${Y}Create certificate for HTTPS support... ${N}";
+seperator;
+/usr/bin/openssl genrsa -out rsa.key 4096;
+/bin/cat /etc/certparams | sed "s/HOSTNAME/`hostname -f`/" | /usr/bin/openssl req -new -key rsa.key -out rsa.csr 2>/dev/null;
+/usr/bin/openssl x509 -req -days 1825 -sha256 -in rsa.csr -signkey rsa.key -out rsa.crt 2>/dev/null;
+cat rsa.key rsa.crt > ${SSL}/ntopng-cert.pem
+rm -f rsa.key rsa.crt rsa.csr;
+# Investigate and add green interface to webinterface access
+echo;
+echo "${Y}Set HTTPS access from green interface only to ntopng WI... ${N}";
+seperator;
+HTTPSPORT=$(awk -F'=' '/GREEN_ADDRESS/ { print"--https-port " $2":"3001 }' ${SETTINGS});
+sed -i "s/--http-port.*/${HTTPSPORT}/" ${CONF};
+
+echo;
+echo "${Y}Will start now ntopng... ${N}";
+seperator;
+/etc/init.d/ntopng start
+
+if pidof -x "ntopng" >/dev/null; then
+    echo;
+    seperator;
+    echo "You can reach ntopngs web interface over ${B}'$(awk -F'=' '/GREEN_ADDRESS/ { print"https://"$2":"3001 }' ${SETTINGS})'${N}";
+    seperator;
+    echo;
+else
+    echo "${R}Process can´t be started please come to https://forum.ipfire.org/viewtopic.php?f=50&t=19565 , try then to help you.${N}";
+fi
 
 # EOF
